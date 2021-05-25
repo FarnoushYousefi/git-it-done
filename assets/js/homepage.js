bdb342c87f4cf4d5f99f75c13b7bfc02a30524e2
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-05-25 14:17:06 -0400

    test

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,51 @@
-var getUserRepos = function () {
-  fetch('https://api.github.com/users/octocat/repos').then(function (response) {
-    console.log('inside', response);
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+  console.log(apiUrl);
+  // make a get request to url
+  fetch(apiUrl).then(function (response) {
+    // console.log(response);
+    response.json().then(function (data) {
+      displayRepos(response, user);
+    });
   });
-
-  console.log('outside');
 };
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+var repoContainerEl = document.querySelector('#repos-container');
+var repoSearchTerm = document.querySelector('#repo-search-term');
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-console.log('outside');
+  // get value from input element
+  var username = nameInputEl.value.trim();
+  console.log(username);
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a GitHub username');
+  }
+  console.log(event);
+};
+var displayRepos = function (repos, searchTerm) {
+ // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+  
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+  
+    // append to container
+    repoEl.appendChild(titleEl);
+  
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+};
+userFormEl.addEventListener('submit', formSubmitHandler);
